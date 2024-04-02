3a29e8dd5e40069d861444e63745a1ed63445ed3
didieravz <didiervz@hotmail.com>
2024-04-02 18:17:41 -0500

    Modificacion inicial my-dashboard

---
+++ src/app/dashboard/pages/defer-view/defer-view.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-defer-view',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './defer-view.component.html',
+  styles: ``
+})
+export default class DeferViewComponent {
+
+}
